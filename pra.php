59d6f3b29adfb25eaefb140894093f04c26f5d93
vinayak <vinayakdarji@gmail.com>
2011-02-02 01:23:08 +0530

    pra.php added

---
+++ pra.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Untitled Document</title>
+</head>
+
+<?php
+
+//$i=10;$j=28;
+//echo "sahil";
+//echo $i+$j;
+
+
+
+
+?>
+
+
+<body>
+<form action="praans.php" method="post">
+
+FirstName:<input type="text" name="FN" />
+Lastname:<input type="text" name="LN" />
+
+<input type="submit" value="submit" />
+
+</form>
+
+
+
+
+
+</body>
+</html>
